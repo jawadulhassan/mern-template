18fc65be13779bd3332518ade0096608bc6ac1de
jawadhassan7 <jawadhassan@folio3.com>
2020-05-09 03:46:43 +0500

    major commit

---
+++ config/index.js
@@ -0,0 +1,6 @@
+// const dbuser = 'booksdummy';
+// const dbpassword = 'Asdf%40123';
+
+const MONGODB_URI = `mongodb+srv://booksdummy:rvQoUwbwsiZevYYD@cluster0-frm2w.mongodb.net/test?retryWrites=true&w=majority`;
+
+module.exports = MONGODB_URI;

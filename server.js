0f3c33ebdf906e161d26dc04a43cd4a5eb9b6fdf
jawadhassan7 <jawadhassan@folio3.com>
2020-05-09 04:52:33 +0500

    port changed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const routes = require('./routes');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // require db connection
 require('./models');

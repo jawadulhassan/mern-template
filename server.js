12479d291a0c02a6a50712f58a2e598f889dcf19
jawadhassan7 <jawadhassan@folio3.com>
2020-05-09 16:26:59 +0500

    minor build middlerware changes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // add this line
-app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.use(routes);
 
